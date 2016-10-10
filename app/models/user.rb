f8f958f2b67abefb1b4f714fa552b135f7cb9c14
Aaron Groch <ar.groch@gmail.com>
2016-10-10 11:15:15 -0400

    day 17 in-class work, tweets and users created, plus the following functionality

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # mount_uploader :avatar, AvatarUploader
+
+  has_many :tweets
+
+  serialize :following, Array
+
+  validates :username, presence: true, uniqueness: true
+end
